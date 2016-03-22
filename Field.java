51f1236f6aaab1280054ba3ba038b44e2745c7c0
Gabriel Tiburcio <tibuurcio@gmail.com>
2016-03-22 09:50:40 +0900

    grass-eating part1

--- Field.java
+++ Field.java
@@ -2,42 +2,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * A Field where wombats live and search for leafs.
- * 
- * @author Gabriel Tibúrcio
- * @version 2016/03/20
  */
 public class Field extends World
 {
 
     /**
-     * Constructs a world with 10x10 cells with a cell 
+     * Constructs a world with 10x10 cells with a cell
      * size of 60x60 pixels and set it's background.
      */
-    public Field() {    
+    public Field() {
         super(10, 10, 60);
         setBackground("cell.jpg");
-        setPaintOrder(Wombat.class, Leaf.class);
-        populate();
-    }
-    
-    /**
-     * Populate the world with a fixed scenario of wombats and leaves.
-     */
-    public void populate() {
-        addObject(new Wombat(), 4, 4);
-        randomLeaves(15);
-    }
-    
-    /**
-     * Place a number of leaves into the world at random places.
-     * The number of leaves can be specified.
-     */
-    public void randomLeaves(int howMany) {
-        for (int i = 0; i < howMany; i++) {
-            Leaf leaf = new Leaf();
-            int x = Greenfoot.getRandomNumber(getWidth());
-            int y = Greenfoot.getRandomNumber(getHeight());
-            addObject(leaf, x, y);
-        }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# grass-eating
+# grass-eating part1
 
-Project to introduce the Greenfoot environment, the act cycle, World and Actor classes, what is an API and the Greenfoot API.
+### Topics to be covered:
+- Presenting the Greenfoot environment
+- Concept of Classes, Objects and the Java language
+- Introducing the World class
+- Introducing variables
+- Creating and interacting with objects in real-time
+
+### To do:
+- Change the size of the world for 10x10x16
+- Change the background of the world for "cell.jpg"
+- Create the Wombat class
+- Create the Leaf class
+- Add the "leavesEaten" parameter as private and initialize it with 0
