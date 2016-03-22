44a5bfecf9f4dc511242678728133a7eedcc9eeb
Gabriel Tiburcio <tibuurcio@gmail.com>
2016-03-22 10:09:31 +0900

    grass-eating part3

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# grass-eating
+# grass-eating part3
 
-Project to introduce the Greenfoot environment, the act cycle, World and Actor classes, what is an API and the Greenfoot API.
+### Topics to be covered:
+- Introducing the greenfoot api methods
+- Removing objects from the World
+- Introducing for loops and adding objects programmatically 
+
+### To do:
+- Check if has found a leaf by using the getOneObjectAtOffset(0, 0, Leaf.class) method
+- Eat leaf by removing it from the world and adding to leavesEaten variable
+- Make a populate method in the Field.java for automatically adding a wombat and leaves to the world
+- Set paint order for the wombat to come first than the leaf
--- Wombat.java
+++ Wombat.java
@@ -2,79 +2,49 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * A wombat that lives in the field and search for leafs.
- * 
+ *
  * @author Gabriel Tibúrcio
- * @version 2016/03/20
  */
 public class Wombat extends Actor
 {
     private int leavesEaten;
-    
+
     public Wombat() {
         leavesEaten = 0;
     }
-    
+
     /**
      * Act - do whatever the Wombat wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() {
-        checkKeyboard();
         if(foundLeaf()) {
             eatLeaf();
         }
-        else if(canMove()) {
-            move();
-        }
-    }  
-    
+        move();
+    }
+
     /**
      * Move the wombat in it's current rotation
      */
     public void move() {
         move(1);
     }
-    
+
     /**
      * Turn left by 90 degrees.
      */
     public void turnLeft() {
         turn(-90);
     }
-    
+
     /**
      * Turn right by 90 degrees.
      */
     public void turnRight() {
         turn(90);
     }
-    
-    /**
-     * Return whether the wombat can move forward or not.
-     */
-    public boolean canMove() {
-        if(!isAtEdge()) {
-            return true;
-        }
-        else {
-            int rotation = getRotation();
-            int width = getWorld().getWidth() - 1;
-            int height = getWorld().getHeight() - 1;
-            int x = getX();
-            int y = getY();
-            if(rotation == 0 && x == width) {
-                return false;
-            } else if(rotation == 90 && y == height) {
-                return false;
-            } else if(rotation == 180 && x == 0) {
-                return false;
-            } else if(rotation == 270 && y == 0) {
-                return false;
-            }
-            return true;
-        }
-    }
-    
+
     /**
      * Check whether there is a leaf in the same cell as we are.
      * Return true if there is, false otherwise.
@@ -88,7 +58,7 @@ public class Wombat extends Actor
             return false;
         }
     }
-    
+
     /**
      * Eat a leaf (if there is one in our cell).
      */
@@ -98,22 +68,10 @@ public class Wombat extends Actor
         if (leaf != null) {
             // eat the leaf...
             getWorld().removeObject(leaf);
-            leavesEaten = leavesEaten + 1; 
+            leavesEaten = leavesEaten + 1;
         }
     }
-    
-    /**
-     * Check keyboard for input of arrow keys
-     */
-    private void checkKeyboard() {
-        if(Greenfoot.isKeyDown("left")) {
-            turnLeft();
-        }
-        if(Greenfoot.isKeyDown("right")) {
-            turnRight();
-        }
-    }
-    
+
     /**
      * Return the number of leafs the wombat has eaten.
      */
